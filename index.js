6916028031c116cd9dac1ccd2a7ab126003f962e
Daniel Castro <dcastro91588@yahoo.com>
2022-11-02 20:33:57 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return function(){
+        return integer * integer;
+    }
+}
+
+const fareDoubler = fare => fare * 2;
+const fareTripler = fare => fare * 3;
+
+function selectDifferentDrivers(drivers, callback){
+    return callback(drivers);
+    // return returnLastTwoDrivers(drivers)
+}
